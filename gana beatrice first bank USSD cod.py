6d2ba7de64bc0186a0ae568c9738d6515c678354
Ganabeatrice <85018939+Ganabeatrice@users.noreply.github.com>
2021-05-29 12:37:03 +0100

    Add files via upload

---
+++ gana beatrice first bank USSD cod.py
@@ -0,0 +1,46 @@
+
+print("welcome to *894#")
+option=input("")
+print("1.>quick banking\n2.>open an account\n3.>loans\n4.>firstmonie")
+option=input("")
+if (option=="1"):
+    amount=input("Enter Amount")
+    pin=input("Enter pin")
+    print("great recharge complete")
+    back=input("Enter 1 To go back to menu")
+    print("Enter 2 to next")
+    option=input("")
+    if (option=="2"):
+    		amount=input("Enter Amount")
+    		number=input("Enter reciver phone  number")
+    		pin=input("Enter pin")
+    		print("recharge complete")
+    		option=input("")
+    		if (option=="3"):
+    			print("1.500mb=500N\n2.1000)=1000")
+    			if (option=="1"):
+    			 amount=input("Enter Amount")
+    			number=input("Enter reciver phone number")
+    			print("Enter 1 to confrim")
+    			confirm=input("press 1")
+    			print("data recharge complete")
+    			option=input("")
+    			if (option=="4"):
+    				account_number=input("Enter the reciver acct number")
+    			print(" the name of the account is GANA BEATRICE")
+    			amount=input("Enter Amount")
+    			confirm_amount=input("Enter 1 to continue")
+    			pin=input("Enter four secret pin")
+    			print("transfer complete")
+    			print("thanks for banking with first bank")
+    			option=input("")
+    			if(option=="5"):
+    				account_number=input("Enter Account Number")
+    				print("1.union bank\n2.stanbic bank\n3.UBA\n4.polarise bank\n5.sky bank\n6.Others")
+    				if(option=="1"):
+    					print("the account name is  GANA BEATRICE")
+    					
+    						
+    			
+    		
+    							
